176b9c0664883f4d4697933cf29f0f0d088e4931
sirakan <sirakan@morphos.is>
2021-02-16 23:35:22 +0700

    update oath google

--- app/controllers/api/user/oauths_controller.rb
+++ app/controllers/api/user/oauths_controller.rb
@@ -1,4 +1,4 @@
-class Api::User::OauthsController < ApplicationController
+class Api::User::OauthsController < Api::ApplicationController
   # sends the user on a trip to the provider,
   # and after authorizing there back to the callback url.
   def oauth
@@ -8,8 +8,10 @@ class Api::User::OauthsController < ApplicationController
 
   def callback
     provider = params[:provider]
+
+    # add callback_url
     if @user = login_from(provider)
-      redirect_back_or_to root_path, notice: "Logged in from #{provider.titleize}!"
+      render json: @user.attributes.merge({callback_url: "https://education-media.vercel.app"}), status: :ok
     else
       begin
         @user = create_from(provider)
@@ -20,12 +22,12 @@ class Api::User::OauthsController < ApplicationController
 
         # reset_session clears session[:return_to_url], so calculate the
         # redirect first
-        redirect_back_or_to root_path, notice: "Logged in from #{provider.titleize}!"
+        render json: @user.attributes.merge({callback_url: "https://education-media.vercel.app/price"}), status: :ok
 
         reset_session # protect from session fixation attack
         auto_login(@user)
-      rescue StandardError
-        redirect_back_or_to root_path, alert: "Failed to login from #{provider.titleize}!"
+      rescue StandardError => e
+        raise AuthenticationError.new(e)
       end
     end
   end
